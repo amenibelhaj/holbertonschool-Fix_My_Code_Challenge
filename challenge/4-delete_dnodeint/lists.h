701a430ce333d297f2e5e072c03626b812cd5a81
amenibelhaj <blameni073@gmail.com>
2025-09-30 14:51:56 +0200

    test

--- challenge/4-delete_dnodeint/lists.h
+++ challenge/4-delete_dnodeint/lists.h
@@ -1,24 +1,47 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-
-#include <stddef.h>
+#include "lists.h"
 
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
+ * delete_dnodeint_at_index - deletes the node at index index
+ * @head: pointer to the head of the doubly linked list
+ * @index: index of the node to delete (starting at 0)
+ *
+ * Return: 1 if it succeeded, -1 if it failed
  */
-typedef struct dlistint_s
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-} dlistint_t;
+    dlistint_t *current;
+    unsigned int i;
 
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head);
-size_t print_dlistint(const dlistint_t *h);
+    if (!head || !*head)
+        return (-1);
 
-#endif /* _LISTS_H_ */
+    current = *head;
+
+    /* Deleting the head node */
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head)
+            (*head)->prev = NULL;
+        free(current);
+        return (1);
+    }
+
+    /* Traverse to the node to delete */
+    for (i = 0; current && i < index; i++)
+        current = current->next;
+
+    if (!current)
+        return (-1);
+
+    /* Update the previous node's next pointer */
+    if (current->prev)
+        current->prev->next = current->next;
+
+    /* Update the next node's prev pointer */
+    if (current->next)
+        current->next->prev = current->prev;
+
+    free(current);
+    return (1);
+}
